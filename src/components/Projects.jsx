68de47c3ca45b82c328d8fb1e3d886edf66d30e8
Rahul <rahulgoswami1523122@gmail.com>
2024-12-21 12:52:47 -0800

    third

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,29 +16,29 @@ const Projects = () => {
       title: 'Portfolio',
       desc: ' This is my portfolio project which is fully responsive , I have completed this project using Reactjs , Tailwind Css  ',
       image: port,
-      live: "https://rohitsingh93300-portfolio.vercel.app/",
-      github: "https://github.com/rohitsingh93300/portfolio"
+      live: "https://my-portfolio-khaki-iota-71.vercel.app/",
+      github: "https://github.com/RAHUL-46/MY-PORTFOLIO"
     },
     {
       title: 'Soft Drink',
       desc: ' I have created this responsive landing page for a soft drink using React js , featuring smooth animations with framer motion .',
       image: fanta,
-      live: "https://spicybites.netlify.app/",
-      github: "https://github.com/rohitsingh93300/YtSpicyBites"
+      live: "https://soft-drink-two.vercel.app//",
+      github: "https://github.com/RAHUL-46/soft-drink"
     },
     {
       title: 'Interior Design ',
       desc: 'I have created this responsive landing page for a Interior Design using React js , Tailwind css ',
       image: estate,
-      live: "https://www.webelitebuilders.com/",
-      github: "#"
+      live: "#",
+      github: "https://github.com/RAHUL-46/HOME-INTERIOR-DESIGN"
     },
     {
       title: 'Gym Landing Page',
       desc: 'I have created this responsive landing page for a Gym using React js , featuring smooth animations with framer motion .',
       image: yoga,
      
-      github: "https://github.com/rohitsingh93300/YouTube-clone"
+      github: "https://github.com/RAHUL-46/GYM-Landing-Page"
     },
   
     {
@@ -46,13 +46,13 @@ const Projects = () => {
       desc: 'This is an Employee Management System project which is fully functional with a admin and employee dashboard using react , tailwind css and context api ',
       image: ems,
      
-      github: "https://github.com/rohitsingh93300/supercars"
+      github: "https://github.com/RAHUL-46/EMS"
     },
     {
       title: 'Myntra Clone',
       desc: ' This is Myntra Clone project which has several functionlities  , I have completed this project using Reactjs , Bootstrap and Redux toolkit , In this project we fetching the data from backend    ',
       image: myntra,
-      github: "https://github.com/rohitsingh93300/portfolio"
+      github: "#"
     },
   ];
 
